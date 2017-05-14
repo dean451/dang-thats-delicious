62cbe9c0f33893f7f7985a5d19a962091c9cf9b9
Dean Palm <dean451@gmail.com>
2017-05-14 01:50:38 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+  const dean = { name: "Dean", age: 26, cool: true };
+  res.send('Oh boy it sure does!');
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+});
+
+
+module.exports = router;
